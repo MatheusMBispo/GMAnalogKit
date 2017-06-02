10ba78d8a01a8efe660dd9ab540ed5805cf6bd9b
Matheus Bispo <matheus.mbispo@gmail.com>
2017-06-02 15:27:23 -0300

    Creating the project

---
+++ GMAnalogKit/GMAnalogKit.h
@@ -0,0 +1,19 @@
+//
+//  GMAnalogKit.h
+//  GMAnalogKit
+//
+//  Created by Matheus Bispo on 02/06/17.
+//  Copyright © 2017 MatheusBispo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GMAnalogKit.
+FOUNDATION_EXPORT double GMAnalogKitVersionNumber;
+
+//! Project version string for GMAnalogKit.
+FOUNDATION_EXPORT const unsigned char GMAnalogKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GMAnalogKit/PublicHeader.h>
+
+
